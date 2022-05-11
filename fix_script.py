b1962311819e2824a51e10c31961983d4bba84c5
HardRope <61661973+HardRope@users.noreply.github.com>
2022-05-12 00:47:59 +0300

    Global changes
    Add get_lessonss() func
    Discard 1 "try - exeption' unit
    Сlarified exception handling in get_child()
    Some edits to match pep8

--- fix_script.py
+++ fix_script.py
@@ -6,30 +6,38 @@ from datacenter.models import Subject
 from datacenter.models import Lesson
 from datacenter.models import Commendation
 
-def get_child_record(name):
+from random import choice
+
+from django.core.exceptions import ObjectDoesNotExist, MultipleObjectsReturned
+
+
+def get_child(name):
     try:
-        child_record = Schoolkid.objects.filter(full_name__contains=name).get()
-    except:
-        print('''
-Несуществующее имя, либо больше одного ученика с таким именем,\
-уточните имя (введите ФИО)
-''')
-        return
-    return child_record
+        child_record = Schoolkid.objects.get(full_name__contains=name)
+        return child_record
+    except ObjectDoesNotExist:
+        print('\nНесуществующее имя, введите ФИО\n')
+    except MultipleObjectsReturned:
+        print('\nНайдено больше одного ученика, уточните ФИО\n')
+
+
+def get_lessons():
+    subjects_title = set()
+    for subject in Subject.objects.all():
+        subjects_title.add(subject.title)
+    return list(subjects_title).sort()
 
 
 def choice_praise():
-    from random import choice
-
-    with open('praises.txt' , 'r', encoding = 'utf-8') as file:
+    with open('praises.txt', 'r', encoding='utf-8') as file:
         praises = [line.rstrip() for line in file]
 
     return choice(praises)
 
 
 def fix_marks(name):
-    child = get_child_record(name)
-    marks = Mark.objects.filter(schoolkid = child, points__lte = 3)
+    child = get_child(name)
+    marks = Mark.objects.filter(schoolkid=child, points__lte=3)
 
     for entry in marks:
         entry.points = 5
@@ -37,46 +45,41 @@ def fix_marks(name):
 
 
 def remove_chastisements(name):
-    child = get_child_record(name)
-    comments = Chastisement.objects.filter(schoolkid = child)
+    child = get_child(name)
+    comments = Chastisement.objects.filter(schoolkid=child)
     comments.delete()
 
 
 def create_commendation(name, lesson_name):
-    child = get_child_record(name)
-    
-    
+    child = get_child(name)
+
+    existing_lessons = get_lessons()
+
+    if lesson_name not in existing_lessons:
+        print('\nНеправильное название предмета. Выберите из списка:\n')
+        print(*existing_lessons, sep=', ')
+        return
+
     lessons = Lesson.objects.filter(
-            year_of_study = child.year_of_study, 
-            group_letter = child.group_letter, 
-            subject__title = lesson_name
+            year_of_study=child.year_of_study,
+            group_letter=child.group_letter,
+            subject__title=lesson_name
             )
 
     lessons = lessons.order_by('date').reverse()
 
     last_lesson = lessons.first()
 
-    try:
-        lesson_subject = last_lesson.subject
-    except AttributeError:
-        print('\nНеправильное название предмета\nДоступные предметы:')
-
-        subjects_title = set()
-        for subject in Subject.objects.all():
-            subjects_title.add(subject.title)
-        print(*subjects_title, sep = ', ')
-        return
-
     commendation = Commendation.objects.filter(
-            schoolkid = child, 
-            subject = lesson_subject, 
-            teacher = last_lesson.teacher
-            ) 
+            schoolkid=child,
+            subject=lesson_subject,
+            teacher=last_lesson.teacher
+            )
 
     commendation.create(
-            text=choice_praise(), 
-            created = last_lesson.date, 
-            schoolkid = child, 
-            subject = last_lesson.subject, 
-            teacher = last_lesson.teacher
+            text=choice_praise(),
+            created=last_lesson.date,
+            schoolkid=child,
+            subject=last_lesson.subject,
+            teacher=last_lesson.teacher
             )
